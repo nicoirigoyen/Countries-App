6f60fd5152e15252719d47c87b410c48128b3b80
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-07-29 21:19:31 -0300

    test 4

--- client/src/redux/consts.js
+++ client/src/redux/consts.js
@@ -1,4 +1,4 @@
-export const BASE_URL= 'http://localhost:3001/api' 
+export const BASE_URL= 'https://countriesapp23.herokuapp.com/api' 
 export const API_ALL= 'https://restcountries.eu/rest/v2/all' 
 export const GET_COUNTRIES = BASE_URL + '/countries'
-export const FILTER_URL = 'http://localhost:3001/api/filter'
+export const FILTER_URL = 'https://countriesapp23.herokuapp.com/api/filter'
