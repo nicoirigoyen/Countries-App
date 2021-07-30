2c72b7a84ce16bedf1937afc07130beb3712c3eb
nicoirigoyen <nico.irigoyen.terre@hotmail.com>
2021-07-29 21:02:47 -0300

    test 1

--- api/src/app.js
+++ api/src/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   
     console.log(___dirname)
     server.get('*', (req, res) =>
-      res.sendFile(path.join(___dirname, '../client', 'build', 'index.html'))
+      res.sendFile(path.join(___dirname, '/../client', 'build', 'index.html'))
     )
   } else {
     server.get('/', (req, res) => {
